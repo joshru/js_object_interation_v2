b4f7802e27985b16655246d8b00691bb3aeb6a20
Josh <joshru@uw.edu>
2016-02-18 16:05:31 -0800

    Switching machines

--- scripts/main.js
+++ scripts/main.js
@@ -7,6 +7,7 @@
 GLOBALS = {
     canvas:     document.getElementById('gameWorld'),
     ctx:        document.getElementById('gameWorld').getContext('2d'),
+    grid:       undefined,
     fontSize:   16,
     numPebbles: 25,
     gridWidth:  25,
@@ -20,6 +21,9 @@ function Grid(game) {
     this.name = "Grid";
     this.x = 0;
     this.y = 0;
+    this.xWidth = 1728;
+    this.yWidth = 2011;
+    this.bg = ASSET_MANAGER.getAsset("./img/hexGrid1.gif");
 
     Entity.call(this, game, this.x, this.y);
 }
@@ -27,30 +31,40 @@ Player.prototype = new Entity();
 Player.prototype.constructor = Grid;
 
 Grid.prototype.update = function() {
-
+    //if (Key.isDown(Key.RIGHT)) {
+    //    this.x += 5;
+    //}
+    //if (Key.isDown(Key.LEFT)) {
+    //    this.x -= 5;
+    //}
+    //if (Key.isDown(Key.UP)) {
+    //    this.y -= 5;
+    //}
+    //if (Key.isDown(Key.DOWN)) {
+    //    this.y += 5;
+    //}
 };
 
 Grid.prototype.draw = function() {
-    var i;
-
-    GLOBALS.ctx.strokeStyle = "#737373";
-    for (i = 0; i < 800; i += GLOBALS.gridWidth) {
-        GLOBALS.ctx.beginPath();
-        GLOBALS.ctx.moveTo(i, 0);
-        GLOBALS.ctx.lineTo(i, 800);
-        GLOBALS.ctx.stroke();
-        GLOBALS.ctx.closePath();
-    }
-
-    for (i = 0; i < 800; i += GLOBALS.gridWidth) {
-        GLOBALS.ctx.beginPath();
-        GLOBALS.ctx.moveTo(0, i);
-        GLOBALS.ctx.lineTo(800, i);
-        GLOBALS.ctx.stroke();
-        GLOBALS.ctx.closePath();
-    }
-
-
+    //var i;
+    //
+    //GLOBALS.ctx.strokeStyle = "#737373";
+    //for (i = 0; i < this.xWidth; i += GLOBALS.gridWidth) {
+    //    GLOBALS.ctx.beginPath();
+    //    GLOBALS.ctx.moveTo(i, 0);
+    //    GLOBALS.ctx.lineTo(i, this.yWidth);
+    //    GLOBALS.ctx.stroke();
+    //    GLOBALS.ctx.closePath();
+    //}
+    //
+    //for (i = 0; i < this.yWidth; i += GLOBALS.gridWidth) {
+    //    GLOBALS.ctx.beginPath();
+    //    GLOBALS.ctx.moveTo(0, i);
+    //    GLOBALS.ctx.lineTo(this.xWidth, i);
+    //    GLOBALS.ctx.stroke();
+    //    GLOBALS.ctx.closePath();
+    //}
+    GLOBALS.ctx.drawImage(this.bg, this.x, this.y, this.xWidth, this.yWidth);
     Entity.prototype.draw.call(this);
 };
 
@@ -131,9 +145,9 @@ Player.prototype.update = function() {
     for (var i = 0; i < this.game.entities.length; i++) {
         var current = this.game.entities[i];
         if (this.collide(current) && current.name === "Food") {
-            this.radius += current.radius;
-            this.mass   += current.mass + 0.1;
-            this.speed  *= 0.96;
+            this.radius += current.radius * 0.5;
+            this.mass   += current.mass * 0.5;
+            this.speed  *= 0.98;
             this.lastColorEaten = current.color;
 
             current.removeFromWorld = true;
@@ -182,7 +196,12 @@ Player.prototype.scale = function(scale) {
 
         }
     }
-    if (GLOBALS.gridWidth >= 7) GLOBALS.gridWidth *= scale;
+    //if (GLOBALS.gridWidth >= 7) GLOBALS.gridWidth *= scale;
+    if (GLOBALS.grid.xWidth > 665 && GLOBALS.grid.yWidth > 576) {
+        GLOBALS.grid.xWidth *= 0.99;
+        GLOBALS.grid.yWidth *= 0.99;
+    }
+
 };
     //GLOBALS.gridWidth *= scale;
 
@@ -200,9 +219,12 @@ function Food(game) {
     this.color = randomColor();
     this.radius = GLOBALS.pebbleSize/*randomInt(10)*/;
     this.mass = this.radius;
-    this.x = randomRangeInt(5, 795);
-    this.y = randomRangeInt(5, 795);
+    this.x = randomRangeInt(5, GLOBALS.canvas.width - 5);
+    this.y = randomRangeInt(5, GLOBALS.canvas.height - 5);
+    this.fleeRadius = 100;
     //console.log("this x = " + this.x + " | this y = " + this.y);
+
+    this.velocity = {x: Math.random() * 100, y: Math.random() * 100};
 
     Entity.call(this, game, this.x, this.y);
 }
@@ -211,9 +233,30 @@ Food.prototype.constructor = Food;
 
 Food.prototype.update = function() {
     var food;
+
+    this.x += this.velocity.x * this.game.clockTick;
+    this.y += this.velocity.y * this.game.clockTick;
+
     for (this.game.foodOnScreen; this.game.foodOnScreen < GLOBALS.numPebbles; this.game.foodOnScreen++) {
         food = new Food(this.game);
         this.game.addEntity(food);
+    }
+
+
+
+    for (var i = 0; i < this.game.entities.length; i++) {
+        var current = this.game.entities[i];
+        if (current.name === "Player") {
+            var dist = distance(this, current);
+
+            if(dist <= this.fleeRadius) {
+                console.log("running");
+                var difX = (current.x - this.x) / dist;
+                var difY = (current.y - this.y) / dist;
+                this.velocity.x += difX * 1000 / (dist * dist);
+                this.velocity.y += difY * 1000 / (dist * dist);
+            }
+        }
     }
 };
 
@@ -272,12 +315,12 @@ function randomRangeInt(min, max) {
 var Key = {
     _pressed: {},
 
-    UP: 87, //w
+    UP:    87, //w
     RIGHT: 68, //d
-    DOWN: 83, //s
-    LEFT: 65, //a
-    R: 82, //R
-    H: 72, //H
+    DOWN:  83, //s
+    LEFT:  65, //a
+    R:     82, //R
+    H:     72, //H
 
     isDown: function (keyCode) {
         return this._pressed[keyCode];
@@ -315,25 +358,34 @@ window.addEventListener('keydown', function (event) {
 }, false);
 
 // main code starts here
-console.log("fluxing the flux capacitor");
-GLOBALS.canvas = document.getElementById('gameWorld');
+var ASSET_MANAGER = new AssetManager();
 
-GLOBALS.ctx = GLOBALS.canvas.getContext('2d');
+ASSET_MANAGER.queueDownload("./img/hexGrid1.gif");
+ASSET_MANAGER.downloadAll(function() {
+    console.log("fluxing the flux capacitor");
+    GLOBALS.canvas = document.getElementById('gameWorld');
 
-GLOBALS.ctx.font = "" + GLOBALS.fontSize + "px serif";
-var gameEngine = new GameEngine();
-var pebble;
-var bg = new Grid(gameEngine);
-gameEngine.addEntity(bg);
+    GLOBALS.ctx = GLOBALS.canvas.getContext('2d');
 
-for (var i = 0; i < GLOBALS.numPebbles; i++) {
-    pebble = new Food(gameEngine);
-    gameEngine.addEntity(pebble);
-    gameEngine.foodOnScreen++;
-}
 
-var player = new Player(gameEngine);
-gameEngine.addEntity(player);
 
-gameEngine.init(GLOBALS.ctx);
-gameEngine.start();
+    GLOBALS.ctx.font = "" + GLOBALS.fontSize + "px serif";
+    var gameEngine = new GameEngine();
+    var pebble;
+    GLOBALS.grid = new Grid(gameEngine);
+    //var bg = new Grid(gameEngine);
+    gameEngine.addEntity(GLOBALS.grid);
+
+    for (var i = 0; i < GLOBALS.numPebbles; i++) {
+        pebble = new Food(gameEngine);
+        gameEngine.addEntity(pebble);
+        gameEngine.foodOnScreen++;
+    }
+
+    var player = new Player(gameEngine);
+    gameEngine.addEntity(player);
+
+    gameEngine.init(GLOBALS.ctx);
+    gameEngine.start();
+});
+
